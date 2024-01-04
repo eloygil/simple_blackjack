05a26e8d14835d504d39279fa686a0a2f809e22f
Eloy Gil Guerrero <eloy@eloygil.com>
2024-01-04 15:57:12 +0000

    Port to python3
    Get the code ready for python3, other minor fixes.

--- engine.py
+++ engine.py
@@ -4,11 +4,14 @@ import click
 
 DEBUG = True
 
-def winnerPrint(gid, text, w_cards, l_cards, remaining, money):
+def debugPrint(msg):
     if DEBUG:
-        print "[ Game #" + str(gid) + " ] " + text + " (" + str(sum(w_cards)) + " vs. " + str(sum(l_cards)) + ") | " + str(w_cards) + " vs. " + str(l_cards) + " | Remaining cards: " + str(remaining) + " | New balance: " + str(money)
-    elif gid % 100 == 0:
-        print "[ Game #" + str(gid) + " ] " + text + " (" + str(sum(w_cards)) + " vs. " + str(sum(l_cards)) + ")"
+        print(msg)
+
+def winnerPrint(gid, text, w_cards, l_cards, remaining, money):
+    debugPrint("[ Game #" + str(gid) + " ] " + text + " (" + str(sum(w_cards)) + " vs. " + str(sum(l_cards)) + ") | " + str(w_cards) + " vs. " + str(l_cards) + " | Remaining cards: " + str(remaining) + " | New balance: " + str(money))
+    if not DEBUG and gid % 100 == 0:
+        print("[ Game #" + str(gid) + " ] " + text + " (" + str(sum(w_cards)) + " vs. " + str(sum(l_cards)) + ")")
 
 def getCards(n):
     #     A  2  3  4  5  6  7  8  9  10   J   Q   K
@@ -35,6 +38,14 @@ def cardsInit(deck):
     c_bank.append(v)
     return c_bank, c_player, new_deck
 
+def getValueAdjustedHand(cards, card):
+    cards.append(card)
+    if sum(cards) > 21:
+        for idx, v in enumerate(cards):
+            if cards[idx] == 11:
+                cards[idx] = 1
+                break
+    return cards
 
 @click.command()
 @click.argument("num_games", default=1000000)
@@ -53,15 +64,13 @@ def main(num_games, num_decks, credit, initial_bet):
 
         # Deal initial cards for player and bank
         cards_bank, cards_player, cards = cardsInit(cards)
-        if DEBUG:
-            print "[ Start ] Bank: " + str(cards_bank) + " | Player: " + str(cards_player)
+        debugPrint("[ Start ] Bank: " + str(cards_bank) + " | Player: " + str(cards_player))
             
         # Deal until the player stands
         while sum(cards_player) < 12 or (sum(cards_player) < bank_min_score and cards_bank[0] > 6):
             new_card, cards = drawCard(cards)
-            cards_player.append(new_card)
-            if DEBUG:
-                print "[ Card ] Player gets " + str(new_card) + " | Now: " + str(cards_player) + " (" + str(sum(cards_player)) + ")"
+            cards_player = getValueAdjustedHand(cards_player, new_card)
+            debugPrint("[ Card ] Player gets " + str(new_card) + " | Now: " + str(cards_player) + " (" + str(sum(cards_player)) + ")")
 
         # If the player didn't lose, deal cards for the bank
         if sum(cards_player) > 21:
@@ -69,16 +78,8 @@ def main(num_games, num_decks, credit, initial_bet):
         else:
             while sum(cards_bank) < bank_min_score:
                 new_card, cards = drawCard(cards)
-                cards_bank.append(new_card)
-                if sum(cards_bank) > 21:
-                    i = 0
-                    for v in cards_bank:
-                        if v == 11:
-                            cards_bank[i] = 1
-                            break
-                        i += 1
-                if DEBUG:
-                    print "[ Card ] Bank gets " + str(new_card) + " | Now: " + str(cards_bank) + " (" + str(sum(cards_bank)) + ")"
+                cards_bank = getValueAdjustedHand(cards_bank, new_card)
+                debugPrint("[ Card ] Bank gets " + str(new_card) + " | Now: " + str(cards_bank) + " (" + str(sum(cards_bank)) + ")")
             if sum(cards_bank) > 21:
                 player_won = True
             elif sum(cards_bank) > sum(cards_player):
@@ -101,8 +102,8 @@ def main(num_games, num_decks, credit, initial_bet):
         else:
             draws += 1
             winnerPrint(game_id, 'Draw', cards_bank, cards_player, cards, money)
-    print "[ Winrate summary ] " + str(float(player_wins * 100.0 / game_id)) + "% WIN | " + str(float(bank_wins * 100.0 / game_id)) + "% LOSE | " + str(float(draws * 100.0 / game_id)) + "% DRAW | Played " + str(game_id) + " games."
-    print "[ Balance summary ] Credit: " + str(credit) + " | Bet: " + str(initial_bet) + " | Profit/Losses: " + ["", "+"][money-credit > 0] + str(money-credit)
+    print("[ Winrate summary ] " + str(float(player_wins * 100.0 / game_id)) + "% WIN | " + str(float(bank_wins * 100.0 / game_id)) + "% LOSE | " + str(float(draws * 100.0 / game_id)) + "% DRAW | Played " + str(game_id) + " games.")
+    print("[ Balance summary ] Credit: " + str(credit) + " | Bet: " + str(initial_bet) + " | Profit/Losses: " + ["", "+"][money-credit > 0] + str(money-credit))
 
 if __name__ == "__main__":
     main()
